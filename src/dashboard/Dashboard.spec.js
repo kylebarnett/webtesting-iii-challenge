28fe0156743dea87b18400dec21de931ccacec63
Kyle Barnett <kbarnett23@gmail.com>
2019-10-02 19:53:23 -0600

    test for controls and display working

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,24 @@
 // Test away
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import Controls from '../controls/Controls';
+import Display from '../display/Display';
+import Dashboard from "./Dashboard";
+
+test("Controls renders without crashing", () => {
+  const toggleLocked = jest.fn()
+  const toggleClosed = jest.fn()
+  render(<Controls
+    locked={false}
+    closed={false}
+    toggleLocked={toggleLocked}
+    toggleClosed={toggleClosed}
+  />);
+});
+
+test("Display renders without crashing", () => {
+  render(<Display
+    locked={false}
+    closed={false}
+  />);
+});
