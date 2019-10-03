b67f28eb86e5c27b9d411e19921a918e9703fc99
Kyle Barnett <kbarnett23@gmail.com>
2019-10-02 21:12:02 -0600

    ran one test that took forever

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -5,6 +5,8 @@ import Controls from '../controls/Controls';
 import Display from '../display/Display';
 import Dashboard from "./Dashboard";
 
+//Make sure controls renders correctly
+
 test("Controls renders without crashing", () => {
   const toggleLocked = jest.fn()
   const toggleClosed = jest.fn()
@@ -16,9 +18,22 @@ test("Controls renders without crashing", () => {
   />);
 });
 
+//Make sure display renders correctly
+
 test("Display renders without crashing", () => {
   render(<Display
     locked={false}
     closed={false}
   />);
 });
+
+//Gate - defaults to unlocked and open & cannot be open or closed if it is locked.
+
+test("defaults to unlocked and open", () => {
+  const { getByText } = render(<Display
+  />);
+  const getUnlockedText = getByText(/unlocked/i)
+  const getOpenText = getByText(/open/i)
+});
+
+//Display component part
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,21 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import Display from './Display';
+
+describe("displays 'Closed' if the closed prop is true and 'Open' if otherwise", () => {
+  it("displays closed", () => {
+    const { getByText } = render(<Display
+      closed={true}
+    />);
+    let closed = getByText(/closed/i)
+    expect(closed.textContent).toBe("Closed")
+  })
+  it("open otherwise", () => {
+    const { getByText } = render(<Display
+      closed={false}
+    />);
+    let open = getByText(/open/i)
+    expect(open.textContent).toBe("Open")
+  })
+})
